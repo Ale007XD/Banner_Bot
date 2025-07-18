168802a8e73ef9103108f026dbb8ec7f552a707a
Ale007XD <alex.deloverov@gmail.com>
2025-07-18 14:57:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,9 +20,7 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
     
-    application.bot.set_my_commands([
-        ('start', BTN_RESTART)
-    ])
+    application.bot.set_my_commands([('start', BTN_RESTART)])
 
     conv_handler = ConversationHandler(
         entry_points=[
@@ -37,17 +35,26 @@ def main() -> None:
                 MessageHandler(Regex(f'^{BTN_TEXT_COLOR}$'), ask_for_color),
                 MessageHandler(Regex(f'^{BTN_FONT}$'), ask_for_font),
                 MessageHandler(Regex(f'^{BTN_TEXT_LINES}$'), ask_for_line_count),
-                # --- ИЗМЕНЕНИЕ ЗДЕСЬ: Добавляем новый обработчик ---
                 MessageHandler(Regex(f'^{BTN_POSTPRINT}$'), ask_for_postprint),
+                # --- ИЗМЕНЕНИЕ ЗДЕСЬ: Добавляем новый обработчик ---
+                MessageHandler(Regex(f'^{BTN_EDIT_TEXT}$'), ask_which_line_to_edit),
                 MessageHandler(Regex(f'^{BTN_GENERATE}$'), generate_banner),
             ],
+            # --- ИЗМЕНЕНИЕ ЗДЕСЬ: Добавляем новые состояния ---
+            AWAIT_LINE_CHOICE_FOR_EDIT: [
+                MessageHandler(Regex(f'^{BTN_BACK}$'), back_to_main_menu),
+                MessageHandler(filters.TEXT & ~filters.COMMAND, ask_for_new_text)
+            ],
+            AWAIT_NEW_TEXT: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, save_edited_text)
+            ],
+            # --- Остальные состояния без изменений ---
             AWAIT_WIDTH: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_width)],
             AWAIT_HEIGHT: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_height)],
             AWAIT_BG_COLOR: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_color)],
             AWAIT_FONT_CHOICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_font)],
             AWAIT_LINE_COUNT: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_line_count_and_ask_text)],
             AWAIT_TEXT_LINES: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_text_and_continue)],
-            # --- ИЗМЕНЕНИЕ ЗДЕСЬ: Добавляем новое состояние ---
             AWAIT_POSTPRINT: [MessageHandler(filters.TEXT & ~filters.COMMAND, save_postprint)],
             PREVIEW_CONFIRM: [
                 CallbackQueryHandler(generate_pdf_callback, pattern="^generate_pdf$"),
